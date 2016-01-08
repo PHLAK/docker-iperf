0580c4373b490b36fce4b08d43b02f47430e3be2
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2016-01-07 19:53:18 -0700

    Build script now always tags created image as 'local'

--- build.sh
+++ build.sh
@@ -7,11 +7,8 @@ IMAGE_NAME="phlak/iperf"
 # Set script directory path
 SCRIPT_DIR="$(dirname $(readlink -f ${0}))"
 
-# Set tag from git commit hash
-TAG="$(git rev-parse --short HEAD)"
-
 # Build the image
-docker build --force-rm --pull --tag ${IMAGE_NAME}:${TAG} ${SCRIPT_DIR}
+docker build --force-rm --pull --tag ${IMAGE_NAME}:local ${SCRIPT_DIR}
 
 # Notify user of success
-echo "Sucessfully created image: ${IMAGE_NAME}:${TAG}"
+echo "Sucessfully created image: ${IMAGE_NAME}:local"
